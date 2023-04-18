4ce54ba2121f058db781b88c19c86914b926cb01
deboik193 <deboik193@gmail.com>
2023-04-18 15:06:45 +0100

    Created the main part of the homepage that keeps the layout from the wireframe:

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './index.css';
+import showItems from './modules/showItems';
+
+const baseURL = 'https://api.tvmaze.com/shows/';
+const showAllItem = document.querySelector('.show-all-item');
+
+showItems(showAllItem, baseURL);
