c49e7ed70afe50f5332983b077965c830719d8d1
jeddaa <fathiato@gmail.com>
2023-04-21 16:44:01 +0100

    adding linters check

---
+++ src/test.js
@@ -0,0 +1,8 @@
+// const counta = response.length;
+// if (typeof counta === 'undefined') {
+//   commentHeader.innerHTML = 'Comments (0)';
+// } else if (typeof counta === 'number' && Number.isInteger(counta)) {
+//   commentHeader.innerHTML = `Comments (${counta})`;
+// }
+// const appId = 'UCdL9KYsi0SBeKyTsp1q';
+// const appId = 'WXD5VCEItQhvz4b5mgk3';
